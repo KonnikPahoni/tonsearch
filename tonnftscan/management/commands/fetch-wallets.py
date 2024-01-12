be1f2611b9a6d9d0a175a7b355a576c55fb093b2
Piotr Markielau <pmarkielau@gmail.com>
2024-01-12 10:18:43 +0100

    Improve app

--- tonnftscan/management/commands/fetch-wallets.py
+++ tonnftscan/management/commands/fetch-wallets.py
@@ -16,11 +16,16 @@ class Command(BaseCommand):
 
         owners_list = NFT.objects.all().values_list("owner", flat=True).distinct()
 
-        addresses_filterset = Address.objects.filter(address__in=owners_list)
+        last_fetched_at = timezone.now() - timezone.timedelta(days=3)
+
+        addresses_filterset = Address.objects.filter(address__in=owners_list, updated_at__lte=last_fetched_at)
         logging.info(f"Found {addresses_filterset.count()} addresses to fetch.")
 
         for wallet in addresses_filterset:
-            fetch_address_service(wallet)
+            try:
+                fetch_address_service(wallet)
+            except Exception as e:
+                logging.error(f"Failed to fetch {wallet.address} with error: {e}")
             time.sleep(1)
 
         time_end = timezone.now()
