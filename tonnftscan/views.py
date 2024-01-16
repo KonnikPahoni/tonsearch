d4c650f15ee8e00f450f0ffcc9244c6a6b9b1f3c
Piotr Markielau <pmarkielau@gmail.com>
2024-01-16 17:51:07 +0100

    Improve app

--- tonnftscan/views.py
+++ tonnftscan/views.py
@@ -76,7 +76,7 @@ class CollectionsView(APIView):
 
         base_context = get_base_context()
 
-        collections_filterset = Collection.objects.filter(last_fetched_at__isnull=False, nfts_count__gt=0).order_by("nfts_count")
+        collections_filterset = Collection.objects.filter(last_fetched_at__isnull=False, nfts_count__gt=0).order_by("-nfts_count")
         # Sort by the number of addresses that own NFTs from the collection
         # collections_filterset = collections_filterset.annotate(num_addresses=Count("nfts__owner", distinct=True))
         # collections_filterset = collections_filterset.order_by("-num_addresses")
